1bd2a5eda795f23efd491e7c40287c8d5ad67e8d
Dancun Yego <dancunyego@gmail.com>
2026-04-06 10:57:46 +0300

    fix: redirect /index.html to home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -24,6 +24,7 @@ function App() {
             <Sonner />
             <BrowserRouter>
               <Routes>
+                <Route path="/index.html" element={<Navigate to="/" replace />} />
                 <Route path="/" element={<Index />} />
                 <Route path="/app" element={<AppDashboard />} />
                 <Route path="/auth" element={<Auth />} />
